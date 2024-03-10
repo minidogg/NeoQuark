2470ad92b284ac75ef439d6f819dac3837dad201
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-03-09 19:45:44 -0600

    e

--- src/init.js
+++ src/init.js
@@ -3,9 +3,17 @@ const fs = require("fs")
 
 async function tryCreate(dir,data){
     if(await fs.existsSync(path.resolve(dir)))return
-    console.log("Creating file: "+dir)
+    console.log("Creating file: '"+dir+"' from '"+__dirname+"'")
     await fs.writeFileSync(path.resolve(dir),data)
 }
+async function tryCreateDir(dir){
+    if(await fs.existsSync(path.resolve(dir)))return
+    console.log("Creating folder: '"+dir+"' from '"+__dirname+"'")
+    await fs.mkdirSync(path.resolve(dir))
+}
 module.exports.init = async ()=>{
-    tryCreate("../data/accounts.json","{}")
+    await tryCreate("../data/accounts.json",JSON.stringify({metadata:{versionCode:0},data:{}}))
+    await tryCreate("../data/servers.json",JSON.stringify({metadata:{versionCode:0},data:{}}))
+    await tryCreateDir("../data/servers",JSON.stringify({metadata:{versionCode:0},data:{}}))
+
 }
